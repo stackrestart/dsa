fcdf22c37dd58a1a5cf8168e96be2fd1ec34b5e7
stackrestart <stackrestart@gmail.com>
2025-11-05 17:26:47 -0600

    Practised until Problem 15

--- day5.py
+++ day5.py
@@ -42,3 +42,65 @@ for i in range(q+1):
         count = hashmap[query1]
         print(query1, count)
 
+
+# https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    count_a = 0
+    count_o = 0
+    dist_a = 0
+    dist_o = 0
+    for i in apples:
+        dist_a = i + a
+        if dist_a >= s and dist_a <= t:
+            count_a += 1
+    for j in oranges:
+        dist_o = j + b
+        if dist_o >= s and dist_o <= t:
+            count_o += 1
+    print(count_a)
+    print(count_o)
+    
+# https://www.hackerrank.com/challenges/kangaroo/problem
+def kangaroo(x1, v1, x2, v2):
+    if v1 <= v2:
+        return "NO"
+    else:
+        num = x2 - x1
+        vel = v2 - v1
+        if num % vel == 0:
+            return "YES"
+        else:
+            return "NO"
+
+# https://www.hackerrank.com/challenges/between-two-sets/problem?isFullScreen=true
+def getTotalX(a, b):
+    max_a = max(a)
+    max_b = max(b)
+    count_possible = 0
+    for i in range(max_a, max_b + 1):
+        good_i = True
+        for j in a:
+            if i % j != 0:
+                good_i = False
+        for k in b:
+            if k % i != 0:
+                good_i = False
+        if good_i == True:
+            count_possible += 1
+    return count_possible
+
+# https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem?isFullScreen=true
+def breakingRecords(scores):
+    min_s = scores[0]
+    max_s = scores[0]
+    min_count = 0
+    max_count = 0
+    for i in scores:
+        if i < min_s:
+            min_s = min(min_s, i)
+            min_count += 1
+        if i > max_s:
+            max_s = max(max_s, i)
+            max_count += 1   
+    return [max_count, min_count]
+    
