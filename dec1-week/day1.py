e85ca5a7d3e8a613723275aab71ce71a5f8c81d3
stackrestart <stackrestart@gmail.com>
2025-12-01 13:02:41 -0600

    .

--- dec1-week/day1.py
+++ dec1-week/day1.py
@@ -5,3 +5,23 @@ def missingNumbers(arr, brr):
         if i in brr:
             brr.remove(i)
     return sorted(list(set(brr)))
+
+# https://www.hackerrank.com/challenges/closest-numbers/problem?isFullScreen=true
+def closestNumbers(arr):
+    # Write your code here
+    arr_srt = sorted(arr)
+ 
+    res = []
+    pairs = {}
+ 
+    for i in range(0, len(arr_srt) - 1):
+        pairs[i] = abs(arr_srt[i] - arr_srt[i+1])
+ 
+    min_diff = min(pairs.values())
+ 
+    for k, v in pairs.items():
+        if v == min_diff:
+            res.append(arr_srt[k])
+            res.append(arr_srt[k + 1])
+ 
+    return res
