98e9c4b880f7c2bcb2cd670b8fa9f6a8fa32a02d
stackrestart <stackrestart@gmail.com>
2025-12-02 22:13:09 -0600

    .

--- dec1-week/day2.py
+++ dec1-week/day2.py
@@ -30,3 +30,14 @@ class Solution:
                 arr.append(str(i))
         return arr
         
+# https://leetcode.com/problems/two-sum/description/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {} 
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in num_dict:
+                return [num_dict[diff], i]
+            num_dict[num] = i
+        return []
