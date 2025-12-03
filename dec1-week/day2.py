7b1135a2354162f219bec8d51c6ce8ef87404f49
stackrestart <stackrestart@gmail.com>
2025-12-02 22:13:58 -0600

    .

--- dec1-week/day2.py
+++ dec1-week/day2.py
@@ -41,3 +41,15 @@ class Solution:
                 return [num_dict[diff], i]
             num_dict[num] = i
         return []
+    
+# https://leetcode.com/problems/reverse-string/description/
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            s[l], s[r] = s[r], s[l]  
+            l += 1
+            r -= 1
+        return s
