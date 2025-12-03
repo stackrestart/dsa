2278969416258833018f26f100236a78ba033435
stackrestart <stackrestart@gmail.com>
2025-12-02 22:12:33 -0600

    .

--- dec1-week/day2.py
+++ dec1-week/day2.py
@@ -5,3 +5,28 @@ class Solution:
             return num
         else:
             return (num - 1) % 9 + 1
+        
+# https://leetcode.com/problems/minimum-number-game/description/
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        arr =[]
+        nums.sort()
+        for i in range(0,len(nums),2):
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        return nums
+
+# https://leetcode.com/problems/fizz-buzz/
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        arr = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                arr.append("FizzBuzz")
+            elif i % 3 == 0:
+                arr.append("Fizz")
+            elif i % 5 == 0:
+                arr.append("Buzz")
+            else:
+                arr.append(str(i))
+        return arr
+        
